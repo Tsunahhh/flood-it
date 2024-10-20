2bd9e6b0f5e1434e256df8c57587720f0a5b33b2
Thomas Malfroid 60552 <60552@etu.he2b.be>
2024-10-20 12:39:25 +0200

    Can start qtapplication with a view of the floodit game

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
+#include <iostream>
 #include <QApplication>
 #include <QPushButton>
 #include <QLabel>
 
 #include "Grid.h"
 #include "GameView.h"
+#include "Game.h"
 
 int main(int argc, char *argv[]) {
+
     QApplication app(argc, argv);
-    model::Grid grid {20, 20, 7};
-    view::GameView view{nullptr, 10, 10};
+
+
+    const model::Settings settings = {10, 10, 4};
+
+    model::Game game{settings};
+
+    view::GameView view{ &game, nullptr, 10, 10};
     view.show();
     return QApplication::exec();
 }
