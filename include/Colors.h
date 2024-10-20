9a8a05800ff856d44d073235409fdfcec3dc43cc
Thomas Malfroid 60552 <60552@etu.he2b.be>
2024-10-20 12:37:23 +0200

    Color and Settings structs

--- include/Colors.h
+++ include/Colors.h
@@ -4,16 +4,33 @@
 
 #ifndef COLORS_H
 #define COLORS_H
+
 namespace model {
     struct Color {
-        int RED{0};
-        int GREEN{1};
-        int YELLOW{2};
-        int ORANGE{3};
-        int PURPLE{4};
-        int CYAN{5};
-        int BLUE{6};
-        int ROSE{7};
+        static constexpr int RED{0};
+        static constexpr int GREEN{1};
+        static constexpr int YELLOW{2};
+        static constexpr int ORANGE{3};
+        static constexpr int PURPLE{4};
+        static constexpr int CYAN{5};
+        static constexpr int BLUE{6};
+        static constexpr int ROSE{7};
+        static constexpr int BLACK{8};
+        static constexpr int WHITE{9};
+
+        int value;
+
+        Color(const int value) : value(value) {}
+
+        int getIdValue() const {
+            return this->value;
+        }
+
+        bool operator==(const Color & color) const {
+            return value == color.value;
+        };
+
     };
 }
+
 #endif //COLORS_H
--- include/Settings.h
+++ include/Settings.h
@@ -6,7 +6,9 @@
 #define SETTINGS_H
 namespace model {
     struct Settings {
-
+        int width{10};
+        int height{10};
+        int colors{6};
     };
 }
 #endif //SETTINGS_H
