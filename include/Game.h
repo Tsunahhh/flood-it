57b59350014f3aab205585423591989a7006a68c
Thomas Malfroid 60552 <60552@etu.he2b.be>
2024-10-20 12:35:38 +0200

    Implementation of Game to work in the view

--- include/Game.h
+++ include/Game.h
@@ -10,12 +10,18 @@
 
 namespace model {
     class Game {
-    private:
+
         int _score;
+        int _max_score;
         Settings _settings;
         Grid _grid;
+        void calculateMaxScore();
     public:
-        explicit Game(Settings settings);
+        Game();
+        explicit Game(const Settings & settings);
+        void play(const int & x, const int & y);
+        Color getColor(const int & x, const int & y) const;
+
     };
 }
 #endif //GAME_H
--- src/model/Game.cpp
+++ src/model/Game.cpp
@@ -1,5 +1,41 @@
-#include "../../include/Grid.h"
-#include "../../include/Settings.h"
-//
-// Created by tsuna on 24/09/24.
-//
+#include <iostream>
+#include <ostream>
+
+#include "Grid.h"
+#include "Settings.h"
+#include "Game.h"
+
+model::Game::Game() :
+    _grid{10, 10, 4},
+    _score(0),
+    _max_score(25),
+    _settings(model::Settings())
+{
+    std::cout << "Game constructor default" << std::endl;
+}
+
+model::Game::Game(const Settings & settings):
+        _grid{settings.height,settings.width, settings.colors},
+        _score(0),
+        _max_score(25),
+        _settings(model::Settings())
+{
+    std::cout << "Game initialized" << std::endl;
+}
+
+
+void model::Game::play(const int &x, const int &y) {
+    const Color selectedColor { _grid.getColor(x, y) };
+    _grid.playColor(selectedColor);
+}
+
+model::Color model::Game::getColor(const int & x, const int & y) const {
+    return _grid.getColor(x, y);
+}
+
+void model::Game::calculateMaxScore() {
+    _max_score = 25;
+}
+
+
+
